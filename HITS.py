294203d50adf5aa4122132752193dc0770d041cb
pritzzmaga <prithvirajan222@gmail.com>
2022-05-01 15:11:52 +0530

    baseList done

--- HITS.py
+++ HITS.py
@@ -8,17 +8,43 @@ def scanFile():
     web_graph
     node_index = 20
     web_graph.nodes[node_index]['page_content']
+    print(web_graph[10])
     pos = {i: web_graph.nodes[i]['pos'] for i in range(len(web_graph.nodes))}
     print(web_graph)
 
-    print(web_graph.nodes[15]['page_content'])
+    print(web_graph.nodes[30]['page_content'])
 
 def findRootSet(query):
     web_graph = nx.read_gpickle("web_graph.gpickle")
     ansList = []
     for i in range(0, len(web_graph.nodes)):
-        if query in web_graph.nodes[i]:
+        if query.lower() in web_graph.nodes[i]['page_content'].lower():
             ansList.append(i)
 
+    print(ansList)
+    return ansList #this is the root set
+
+
+
+def findBaseSet(rootList):
+    baseList = []
+    web_graph = nx.read_gpickle("web_graph.gpickle")
+    for i in rootList:
+        if i not in baseList:
+            baseList.append(i)
+        for j in web_graph[i]:
+            if j not in baseList:
+                baseList.append(j)
+
+
+    baseList.sort()
+    print(baseList)
+    return baseList
+
+def makeAdjacencyMatrix(baseList):
+    A = np.matrix()
+
+
 scanFile()
-findRootSet()
+rootList = findRootSet('War')
+baseList = findBaseSet(rootList)
