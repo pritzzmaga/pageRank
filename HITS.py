96e4504360754a756fcd53f8df604b2dbd19946b
pritzzmaga <prithvirajan222@gmail.com>
2022-05-02 04:01:27 +0530

    part a completed

--- HITS.py
+++ HITS.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib as plt
+from scipy.linalg import eig
+
 
 def scanFile():
     web_graph = nx.read_gpickle("web_graph.gpickle")
@@ -14,6 +16,7 @@ def scanFile():
 
     print(web_graph.nodes[30]['page_content'])
 
+
 def findRootSet(query):
     web_graph = nx.read_gpickle("web_graph.gpickle")
     ansList = []
@@ -22,8 +25,7 @@ def findRootSet(query):
             ansList.append(i)
 
     print(ansList)
-    return ansList #this is the root set
-
+    return ansList  # this is the root set
 
 
 def findBaseSet(rootList):
@@ -36,15 +38,61 @@ def findBaseSet(rootList):
             if j not in baseList:
                 baseList.append(j)
 
-
     baseList.sort()
     print(baseList)
     return baseList
 
+
 def makeAdjacencyMatrix(baseList):
-    A = np.matrix()
+    web_graph = nx.read_gpickle("web_graph.gpickle")
+    A_index = {}
+    A = np.zeros((len(baseList), len(baseList)), float)
+    count = 0
+    for i in baseList:
+        A_index[i] = count
+        count += 1
+    for i in baseList:
+        for j in web_graph[i]:
+            if j in baseList:
+                A[A_index[i]][A_index[j]] = 1
+
+    return A
+
+
+def findAandH(A):
+    AT = A.T
+    #hw, hv = np.linalg.eig(np.matmul(A, AT))
+    hw, hv = eig(np.matmul(A, AT), left=False, right=True)
+    ma = 0
+    ind = 0
+    for i in range(0, len(hw)):
+        if hw[i] > ma:
+            ind = i
+            ma = hw[i]
+    h = hv[:, ind].real
+
+    print(h*h/sum(h*h))
+    print(abs(sum(h*h/sum(h*h))))
+
+    aw, av = eig(np.matmul(AT, A), left=False, right=True)
+    ma = 0
+    ind = 0
+    for i in range(0, len(aw)):
+        if aw[i] > ma:
+            ind = i
+            ma = aw[i]
+    a = av[:, ind].real
+
+    print(a * a / sum(a * a))
+    print(abs(sum(a * a / sum(a * a))))
+
+
 
 
 scanFile()
 rootList = findRootSet('War')
 baseList = findBaseSet(rootList)
+A = makeAdjacencyMatrix(baseList)
+findAandH(A)
+
+
--- pageRanker.py
+++ pageRanker.py
@@ -1,26 +1,35 @@
 import numpy as np
 import pandas as pd
 from scipy.linalg import eig
+import networkx as nx
 
 
-def getInput(n, k):
+def getInput(n_getInput, k_getInput):
     # used to take input of graph
-    A = np.zeros((n, n), float)
+    G = nx.MultiDiGraph()
+    A = np.zeros((n_getInput, n_getInput), float)
     print("Enter the Edges:")
-    for i in range(0, k):
+    for i in range(0, k_getInput):
         x, y = input().split(',')
-        A[int(x)-1][int(y)-1] = 1
-    return A
+        G.add_edge(int(x), int(y))
+        A[int(x) - 1][int(y) - 1] = float(1.0)
+    return A, G
+
+    # A = [[0.0, 1.0, 0.0, 0.0],
+    #      [1.0, 0.0, 1.0, 0.0],
+    #      [0.0, 1.0, 0.0, 1.0],
+    #      [0.0, 0.0, 1.0, 0.0]]
+    # return A
 
 
-def powerIteration(A):
-    x = np.zeros((1, np.shape(A)[0]), float)
+def powerIteration(A_powerIteration):
+    x = np.zeros((1, np.shape(A_powerIteration)[0]), float)
     x[0][0] = 1.0
     close = False
     x_prev = x
     while not close:
         # print(x)
-        x = np.matmul(x, A)
+        x = np.matmul(x, A_powerIteration)
 
         res = np.subtract(x, x_prev)
         errorInArray = 0.0
@@ -34,45 +43,65 @@ def powerIteration(A):
     return x
 
 
-def rightEigen(A):
-    At = A.T
-    print(At)
+def rightEigen(A_eigen):
+    At = A_eigen.T
+
     abc, At = np.linalg.eig(At)
-    W, vl = eig(A, left=True, right=False)
+    W, vl = eig(A_eigen, left=True, right=False)
     arr1 = vl[:, 0].real
-    print(arr1/sum(arr1))
+    print(arr1 / sum(arr1))
 
 
-def makePageRank(n, k):
-    alpha = 0.1
-    A = getInput(n, k)
+def makePageRank(n, A_alpha, alpha):
+    # alpha = 0.1
+    print(A_alpha)
     sum = 0
     # step 1 check for rows with no 1's and replace full row with 1/N's
     # step 2 divide each 1 by the number of 1's in the row
     for i in range(0, n):
         sum = 0
         for j in range(0, n):
-            sum += A[i][j]
+            sum += A_alpha[i][j]
         if sum == 0:
             for j in range(0, n):
-                A[i][j] = float(1 / n)
+                A_alpha[i][j] = float(1 / n)
         else:
             for j in range(0, n):
-                if A[i][j] == 1:
-                    A[i][j] = float(A[i][j] / sum)
-    print(A)
-    A = A * (1 - alpha)
-    A = A + (alpha) / n
+                if A_alpha[i][j] == 1:
+                    A_alpha[i][j] = float(A_alpha[i][j] / sum)
+    # print(A)
+    A_alpha = A_alpha * (1 - alpha)
+    A_alpha = A_alpha + (alpha) / n
 
-    print(A)
+    print(A_alpha)
+    print("alpha is ")
+    print(alpha)
 
-    x = powerIteration(A)
-    print(x)
-    rightEigen(A)
+    # x = powerIteration(A)
+    # print(x)
+    rightEigen(A_alpha)
     # print(y)
+
+
+def makePageRankAlphaZero(n, A_zero_alpha, G, k):
+    ansList = []
+    print(A_zero_alpha)
+    if nx.is_strongly_connected(G):
+        print("strongly connectted")
+        for i in range(0, n):
+            ansList.append(sum(A_zero_alpha[i])/k)
+        print(ansList)
+    else:
+        print("not strongly connected")
 
 
 if __name__ == '__main__':
     n = int(input("Enter the number of nodes: "))
     k = int(input("Enter the number of connections: "))
-    makePageRank(n, k)
+    A, G = getInput(n, k)
+    A_dummy = A
+
+    makePageRankAlphaZero(n, A, G, k)
+    makePageRank(n, A_dummy, 0.1)
+
+
